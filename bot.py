ebb15ce09e0fe6a9e88b58a141cac49f2ff213bb
Polat <84643819+BirBeyfendi@users.noreply.github.com>
2022-01-24 22:02:24 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -31,9 +31,9 @@ self_or_contact_filter = filters.create(
 @app.on_message(filters.command('start') & self_or_contact_filter)
 async def start(client, message):
       if message.from_user.id not in SUDO_USERS or message.from_user.id != OWNER_ID:
-      return await message.reply_text("Üzgünüm ama bu hesabı kullanamazsın. Kendine [buradan](https://github.com/BirBeyfendi/magnetarvc) bir hesap kurabilirsin!")
+      return await message.reply("Üzgünüm ama bu hesabı kullanamazsın. Kendine [buradan](https://github.com/BirBeyfendi/magnetarvc) bir hesap kurabilirsin!")
       else:
-      await message.reply_text("**Selam!** \n**Ben** `Magnetar Müzik Çalar` **Hesabıyım.** \n**Komutlarım Basittir.** \n\n**Komutlarım:** \n**Start, Ping, Baslat, Beklet, Sesekatil, Sestenayril** 🎵 \n\n**Keyifli Müzik Dinlemeler Dilerim Efendim.**")
+      await message.reply("**Selam!** \n**Ben** `Magnetar Müzik Çalar` **Hesabıyım.** \n**Komutlarım Basittir.** \n\n**Komutlarım:** \n**Start, Ping, Baslat, Beklet, Sesekatil, Sestenayril** 🎵 \n\n**Keyifli Müzik Dinlemeler Dilerim Efendim.**")
 
 # ping kontrolcüsü
 @app.on_message(filters.command('ping') & self_or_contact_filter)
