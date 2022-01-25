08e849d7fb5d6a7180b5516704d471c781d0cb2d
aylak-github <85997548+aylak-github@users.noreply.github.com>
2022-01-25 21:58:52 +0300

    amk

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ from os import getenv
 from dotenv import load_dotenv
 
 load_dotenv()
-    STRING_SESSION = getenv("STRING_SESSION")
-    API_ID = int(getenv("API_ID"))
-    API_HASH = getenv("API_HASH")
-    OWNER_ID = getenv("OWNER_ID")
-    SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
+STRING_SESSION = getenv("STRING_SESSION")
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+OWNER_ID = getenv("OWNER_ID")
+SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
